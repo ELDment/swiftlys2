89a1011c9f0717df05e3cf008ac72976906bb747
samyyc <3356207189@qq.com>
2026-07-09 15:41:50 +0800

    fix(managed): Update protobuf

--- managed/src/SwiftlyS2.Generated/Schemas/ClassConvertor.cs
+++ managed/src/SwiftlyS2.Generated/Schemas/ClassConvertor.cs
@@ -6,8 +6,7 @@ internal static class ClassConvertor
 {
     public static CEntityInstance ConvertEntityByDesignerName( nint address, string designerName )
     {
-        return designerName switch
-        {
+        return designerName switch {
             "func_nav_avoidance_obstacle" => new CFuncNavObstructionImpl(address),
             "wearable_item" => new CEconWearableImpl(address),
             "trigger_detect_explosion" => new CTriggerDetectExplosionImpl(address),
@@ -485,7 +484,6 @@ internal static class ClassConvertor
             "func_combined_light_probe_volume" => new CEnvCombinedLightProbeVolumeImpl(address),
             "info_hostage_spawn" => new CHostageImpl(address),
             "item_defuser" => new CItemDefuserImpl(address),
-            "prop_door_rotating" => new CPropDoorRotatingImpl(address),
             _ => new CEntityInstanceImpl(address),
         };
     }
